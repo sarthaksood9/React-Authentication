c1d4ee25a1c6cb18d106ca443babd9e1ffd96d6c
sarthaksood9 <sarthaksood9@gmail.com>
2023-10-01 12:44:35 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
 import Login from './components/Login';
 
 function App() {
   return (
     <>
-    {/* <Navbar/> */}
     <Login/>
     </>
   );
